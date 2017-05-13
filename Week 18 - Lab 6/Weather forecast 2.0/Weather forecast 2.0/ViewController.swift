7e4c8ecf8da7633a4f8a1f5993be07ed84792d42
Hamza Ghani <h.u.f.ghani@gmail.com>
2017-05-13 16:33:58 +0100

    weather forcaset 2.0

---
+++ Week 18 - Lab 6/Weather forecast 2.0/Weather forecast 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather forecast 2.0
+//
+//  Created by Hamza Ghani on 13/05/2017.
+//  Copyright © 2017 Hamza Ghani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
